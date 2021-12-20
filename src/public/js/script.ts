5c903172fd334fccf830b2f1483fae985610004d
Bear Evans <81526068+bear-evans@users.noreply.github.com>
2021-12-20 12:06:06 -0500

    Update script.ts

--- src/public/js/script.ts
+++ src/public/js/script.ts
@@ -1,9 +1,4 @@
 const deviceGenerator = function () {
-  const nouns = [];
-  const verbs = [];
-  const adjectives = [];
-  const materials = [];
-
   function init() {
     console.log("Device Generator Initialized");
   }
